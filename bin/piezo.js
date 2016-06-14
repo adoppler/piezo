ee512e639de33e5c680f7772d4f865cf5fd3a55b
Andrew Doppler <adoppler@users.noreply.github.com>
2016-06-14 11:33:37 -0600

    Clean up cli

--- bin/piezo.js
+++ bin/piezo.js
@@ -1,82 +1,103 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk')
 const path = require('path')
 
-const task = args = process.argv[2]
+const chalk = require('chalk')
+const cpr = require('cpr')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const rimraf = require('rimraf')
+const webpack = require('webpack')
+
 const config = require('../config')
+const generateSitemap = require('../sitemap/sitemap-generator')
+const webpackConfig = require('../webpack/webpack-config.js')(config)
 
 const input = path.join(config.__root, config.build.source)
 const output = path.join(config.__root, config.build.output)
 
-function checkError(err) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-}
-
-function clean(cb) {
-  const rimraf = require('rimraf')
-
-  rimraf(output, (err) => {
-    checkError(err)
-    cb()
-  })
-}
-
-function build() {
-  process.env['NODE_ENV'] = 'production'
-
-  const cpr = require('cpr')
-  const webpack = require('webpack')
-  const webpackConfig = require('../webpack/webpack-config.js')(config)
-  const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-  const generateSitemap = require('../sitemap/sitemap-generator')
-
-  const startTime = Date.now()
-
-  console.log(chalk.green.bold(`Building static site...`), '\n')
-
-  clean(() => {
-    cpr(path.join(input, 'www'), output, (cprError) => {
-      checkError(cprError)
-
-      const compiler = webpack(webpackConfig)
-
-      compiler.apply(
-        new ProgressBarPlugin({
-          format: '  webpack build  [:bar] :percent',
-          clear: false,
-          width: 60,
-          summary: false
-        })
-      )
-
-      compiler.run((webpackError) => {
-        checkError(webpackError)
-
-        generateSitemap((sitemapError) => {
-          checkError(sitemapError)
-
-          console.log(chalk.green.bold(`Success after (${(Date.now() - startTime) / 1000 + 's'})`))
-        })
-      })
+function clean() {
+  return new Promise((resolve, reject) => {
+    rimraf(output, err => {
+      err ? reject(err) : resolve()
     })
   })
 }
 
-switch (task) {
-  case 'build':
-    build()
-    break
-  case 'clean':
-    clean(() => process.exit(0))
-    break
-  case 'server':
-    require('../server')
-    break
-  default:
-    console.log('Usage: piezo [build|clean|server]')
-    process.exit(1)
+function copy(input, output) {
+  return new Promise((resolve, reject) => {
+    cpr(input, output, err => {
+      err ? reject(err) : resolve()
+    })
+  })
 }
+
+function compile() {
+  process.env['NODE_ENV'] = 'production'
+
+  return new Promise((resolve, reject) => {
+    const compiler = webpack(webpackConfig)
+
+    compiler.apply(
+      new ProgressBarPlugin({
+        format: '  webpack build  [:bar] :percent',
+        clear: false,
+        width: 60,
+        summary: false
+      })
+    )
+
+    compiler.run(err => {
+      err ? reject(err) : resolve()
+    })
+  })
+}
+
+function render() {
+  process.env['NODE_ENV'] = 'production'
+
+  return new Promise((resolve, reject) => {
+    generateSitemap(err => {
+      err ? reject(err) : resolve()
+    })
+  })
+}
+
+function build() {
+  const startTime = Date.now()
+  console.log(chalk.green.bold(`Building static site...`), '\n')
+
+  return clean()
+    .then(() => copy(path.join(input, 'www'), output))
+    .then(compile)
+    .then(render)
+    .then(() => {
+      console.log(chalk.green.bold(`Success after (${(Date.now() - startTime) / 1000 + 's'})`))
+    })
+}
+
+function cli(task) {
+  switch (task) {
+    case 'build':
+      return build()
+    case 'clean':
+      return clean()
+    case 'compile':
+      return compile()
+    case 'render':
+      return render()
+    case 'help':
+      console.log('Usage: piezo [build|clean|compile|render|server]')
+      process.exit(0)
+    case 'server':
+    default:
+      require('../server')
+      return new Promise(() => {})
+  }
+}
+
+cli(process.argv[2])
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
